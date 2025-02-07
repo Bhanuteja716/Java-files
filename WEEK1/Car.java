563786dd909fa92887ccd93de0d28b0c183bdaed
Bhanuteja716 <tejab0985@gmail.com>
2025-02-07 10:29:57 +0530

    Car.java

--- WEEK1/Car.java
+++ WEEK1/Car.java
@@ -1,10 +1,13 @@
-Class Car
+Class car
 {
       String name;
      String model;
       int year;
      public void start()
 {
+      System.out.println("Car name"+name);
+      System.out.println("Car Model"+model);
+      System.out.println("Car year"+year);
      System.out.println("Car Started");
 }
 public void stop()
@@ -14,24 +17,24 @@ public void stop()
 
 
 }
-class Brand
+class Car
 {
     public static void main(String args[])
     {
-        Car car1 = new Car();
+        car car1 = new car();
         System.out.println("First Car Details:");
         car1.name="Toyota";
         car1.model="Corolla";
         car1.year=2020;
 
         car1.start();
-        Car car2 = new Car();
+        car car2 = new Car();
         System.out.println("Second Car Details:");
         car2.name="Honda";
         car2.model="Civic";
         car2.year=2019;
         car2.start();
-        Car car3 = new Car();
+        car car3 = new Car();
         System.out.println("Third Car Details:");
         car3.name="Ford";
         car3.model="Focus";
