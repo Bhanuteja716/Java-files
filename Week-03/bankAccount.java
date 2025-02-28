327bc1d7a2b876adfa45d74a4a640da35c3544e8
Bhanuteja716 <tejab0985@gmail.com>
2025-02-28 08:21:59 +0530

    Add files via upload

--- Week-03/bankAccount.java
+++ Week-03/bankAccount.java
@@ -1,50 +1,44 @@
-import java.util.*;
-class bankAccount
-
-           
-
-   {
-      public String Customer_name; 
-      public String Account_type;
-      public int amount;
-      public int withdraw;
-      public int balance;
-      public int cash;
-      public int deposit;
-
-
-      public void withdrawl()
-      {
-          if(withdraw>balance)
-          System.out.println("Insufficient funds");
-          else
-     
-          System.out.println("Enter withdrawal amount:");
-          withdraw = sc.nextInt();
-          amount = balance-withdraw;
-          
-          System.out.println("Enter balance amount:");
-          balance = sc.nextInt();
-          System.out.println("Withdrawl amount is:"+withdraw);
-          System.out.println("Remaining balance is:"+amount);
-      }
-      public void deposit()
-      {
-         
-          
-         System.out.println("Amount deposited");
-          amount = deposit+balance;
-          System.out.println("Balance is :"+amount);
-      }
-      public static void main(String args[])
-      {
-          Scanner sc = new Scanner(System.in);
-          bankAccount b1 = new bankAccount();
-          b1.withdrawl();
-          
-          bankAccount b2 = new bankAccount();
-   System.out.println("Enter deposit amount:"+cash);
-          int cash = sc.nextInt(); 
-          b2.deposit();
+class BankAccount
+{
+      
+     public String Acchname;
+     public int Accnumber;
+     public float curramount;
+    BankAccount(String Acchname, int Accnum,float curramount)
+    {
+       this.Acchname = Acchname;
+       this.Accnumber = Accnumber;
+       this.curramount = curramount;
+      System.out.println("Enter Account holder name:"+Acchname);
+        System.out.println("Enter Account number:"+Accnum);
+        System.out.println("Enter current amount:"+curramount);
+    }
+    public void withdraw(int withdraw)
+    {
+       if(withdraw>curramount)
+       {
+             System.out.println("Insufficient Funds");
        }
-}
+       else
+       {
+            curramount = curramount-withdraw;
+            System.out.println("withdraw amount is:"+withdraw);
+            System.out.println("Current amount is:"+curramount);
+       }
+   }
+   public void deposit(int deposit)
+      {
+            System.out.println("Deposited amount is :");
+            curramount = curramount+deposit;
+            System.out.println("Deposited amount is:"+deposit);
+            System.out.println("Total current amount is:"+curramount);
+       }
+
+       public static void main(String args[])
+       {
+              
+            BankAccount b = new BankAccount("Hari",24210,100000);   
+            b.deposit(10000);
+            b.withdraw(500);
+
+}}
