5577b7006aca7c7c986fade490267457a8e97ce1
Bhanuteja716 <tejab0985@gmail.com>
2025-03-07 10:32:18 +0530

    Add files via upload

---
+++ week-01/Main.java
@@ -0,0 +1,9 @@
+class Main
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Name: K.R.N Bhanu Teja");
+			System.out.println("Class :CSE-C");
+			System.out.println("Roll No:24210");
+		}
+}
